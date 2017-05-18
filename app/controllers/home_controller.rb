12abafe8dfeeea4e6948e747938a11335d3af3b6
Janginwan <inwanj@naver.com>
2017-05-18 02:19:18 +0000

    인완꺼

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,6 @@
+class HomeController < ApplicationController
+  def index
+    @jpg = "bnw.jpg"
+    @png = "abc.jpg"
+  end
+end
---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,8 @@
+module ApplicationHelper
+
+    def bootstrap_version
+    # CDN으로부터 원하는 Bootstrap 버전을 지정한다.
+    "3.3.2"
+  end
+
+end
