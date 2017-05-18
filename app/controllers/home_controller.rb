72358c67b021a36d64ec4c885be5f164a1bc264b
Janginwan <inwanj@naver.com>
2017-05-18 06:39:53 +0000

    crud게시판작성

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,20 @@
 class HomeController < ApplicationController
   def index
+  @titledata=params[:title]
    
+    @contentdata=params[:content]
+    
+    @content2data=params[:content2]
+    
   end
+  
+  
   
   def home
     unless user_signed_in?
         redirect_to "/index"
-   end
+    end
   end
+  
 end
+    
